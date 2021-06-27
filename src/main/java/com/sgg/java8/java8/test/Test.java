c7a6eaabbd18b121b68c011b373544f3c13bffb4
onepeace <1124626619@qq.com>
2021-06-27 12:45:35 +0800

    git3

--- src/main/java/com/sgg/java8/java8/test/Test.java
+++ src/main/java/com/sgg/java8/java8/test/Test.java
@@ -3,5 +3,6 @@ package com.sgg.java8.java8.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("git22");
     }
 }
